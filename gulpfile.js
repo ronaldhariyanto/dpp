ee20527ac16d3feaad381923a79a57a97e7f6461
Ronald Hariyanto <ronald@Ronalds-MacBook-Pro.local>
2017-12-20 09:03:51 +0700

    adding nuxt

--- gulpfile.js
+++
@@ -1,3 +0,0 @@
-const gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    ejs = require('gulp-ejs');
---
+++ nuxt.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  head: {
+    titleTemplate: '%s - Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'vieport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: '/css/uikit.min.css' },
+      { rel: 'favicon', href: 'favicon.ico' }
+    ],
+    script: [
+      { src: '/js/uikit.min.js' },
+      { src: '/js/uikit-icons.min.js' }
+    ]
+  }
+}
